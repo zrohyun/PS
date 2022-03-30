c4c3033cda05840557a4b36346484f4ff7429b42
0hyunU <ryh2803@gmail.com>
2022-03-30 12:52:16 +0900

    bruteforce silver level 3
    update 1051.py
    다른 풀이 설명 추가

--- Python/baekjoon/1051.py
+++ Python/baekjoon/1051.py
@@ -5,6 +5,10 @@ def solution():
     n,m = list(map(int,input().split()))
     square = [list(str(input())) for _ in range(n)]
 
+    # 정사각형의 크기를 구하는지 몰랐다. 직사각형되 되는 줄 알았다.
+    # 정사각형만 구한다면 크기를 topdown으로 가장 큰 크기부터
+    # 계산해내려오면 된다. 처음 조건에 맞는 정사각형을 찾게된다면
+    # 바로 탐색을 중지할 수 있다.
     def find_square(y,x):
         num = square[y][x]
         size = 1
