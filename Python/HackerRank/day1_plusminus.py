c44ca1100befdbbb1d1ea1c1bcda5ed42ea7b94c
0hyunU <ryh2803@gmail.com>
2022-04-11 03:56:01 +0900

    A day1_plusminus.py

---
+++ Python/HackerRank/day1_plusminus.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+
+
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    p,m,z = 0,0,0
+    for i in arr:
+        if i>0:
+            p +=1
+        elif i<0:
+            m+=1
+        else:
+            z +=1
+    
+    for i in [p,m,z]:
+        print(i/len(arr))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
