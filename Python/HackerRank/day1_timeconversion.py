6b3a5e4821b6308465f87ad22925cdde01f47cbe
0hyunU <ryh2803@gmail.com>
2022-04-12 00:10:57 +0900

    A day1_timeconversion.py

---
+++ Python/HackerRank/day1_timeconversion.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    
+    if s[:2] == '12':
+        if s[-2:] == 'PM': 
+            return s[:-2]
+        else:
+            return '00' + s[2:-2] 
+    
+    if s[-2:] == "AM":
+        return s[:-2]
+    else:
+        return str(int(s[:2])+12) + s[2:-2]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
