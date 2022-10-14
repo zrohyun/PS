df624512f8000a1b8a5460b7bc9c8ee1e5a2cfd2
0hyunU <ryh2803@gmail.com>
2022-10-15 00:10:01 +0900

    M 14499주사위굴리기.py

---
+++ Python/SWEA/14499주사위굴리기.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+N,M,x,y,k = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(N)]
+order = list(map(int, input().split()))
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+dice = [0 for _ in range(7)]
+
+def is_out_of_bound(nx,ny):
+    if 0<= nx < N and 0<= ny <M:
+        return False
+    return True
+
+def move(direction):
+    if direction == 1:
+        dice[1], dice[3], dice[4], dice[6] = dice[3], dice[6], dice[1], dice[4]
+    elif direction == 2:
+        dice[1], dice[3], dice[4], dice[6] = dice[4], dice[1], dice[6], dice[3]
+    elif direction == 3:
+        dice[1], dice[2], dice[5], dice[6] = dice[2], dice[6], dice[1], dice[5]
+    elif direction == 4:
+        dice[1], dice[2], dice[5], dice[6] = dice[5], dice[1], dice[6], dice[2]
+
+
+for i in order:
+    nx = x+dx[i-1]
+    ny = y+dy[i-1]
+
+    if is_out_of_bound(nx,ny):
+        continue
+
+    x, y = nx, ny
+    move(i)
+
+    if board[x][y]:
+        dice[1] = board[x][y]
+        board[x][y] = 0
+    else:
+        board[x][y] = dice[1]
+
+    print(dice[6])
