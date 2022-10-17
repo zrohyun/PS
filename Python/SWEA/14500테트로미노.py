fdd3bb852b07bfe07481dbfdecc9147df97c0dd9
0hyunU <ryh2803@gmail.com>
2022-10-18 00:10:00 +0900

    M 14500테트로미노.py

---
+++ Python/SWEA/14500테트로미노.py
@@ -0,0 +1,51 @@
+"""
+condition
+N,M [4,500]
+숫자R under 1000
+
+"""
+# 입력 받기
+import sys; input = sys.stdin.readline
+
+N,M = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(N)]
+visit = [([0] * M) for _ in range(N)]
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+ans = 0
+max_val = max(map(max, board))
+
+def dfs(r, c, idx, total):
+    global ans
+
+    # 이른 종료
+    if ans >= total + max_val * (3 - idx):
+        return
+    if idx == 3:
+        ans = max(ans, total)
+        return
+    else:
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < N and 0 <= nc < M and visit[nr][nc] == 0:
+                if idx == 1:
+                    visit[nr][nc] = 1
+                    dfs(r, c, idx + 1, total + board[nr][nc])
+                    visit[nr][nc] = 0
+                visit[nr][nc] = 1
+                dfs(nr, nc, idx + 1, total + board[nr][nc])
+                visit[nr][nc] = 0
+
+for r in range(N):
+    for c in range(M):
+        visit[r][c] = 1
+        dfs(r, c, 0, board[r][c])
+        visit[r][c] = 0
+
+print(ans)
+
+
+
+
+
